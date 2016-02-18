d9eba013dfaea5f7ee0511a5df87a5648976d286
Antoine Sauray <sauray.antoine@gmail.com>
2016-02-18 20:05:18 +0100

    fixed indent

--- src/io.c
+++ src/io.c
@@ -1,25 +1,25 @@
 #include "../include/io.h"
 
 FILE* openFile(char* path, char* mode){
-  FILE *f = fopen(path, mode);
-  if (f == NULL){
-      printf("Failed to open file %s\n", path);
-      return NULL;
-  }
-  return f;
+      FILE *f = fopen(path, mode);
+      if (f == NULL){
+            printf("Failed to open file %s\n", path);
+            return NULL;
+      }
+      return f;
 }
 void closeFile(FILE* f){
-    fclose(f);
+      fclose(f);
 }
 bool isLetter(char c){
-    if((c > 'a' && c < 'z') || (c > 'A' && c < 'Z')){
-        return true;
-    }
-    return false;
+      if((c > 'a' && c < 'z') || (c > 'A' && c < 'Z')){
+            return true;
+      }
+      return false;
 }
 bool isNumber(char c){
-    if(c > '0' && c < '9'){
-        return true;
-    }
-    return false;
+      if(c > '0' && c < '9'){
+            return true;
+      }
+      return false;
 }
