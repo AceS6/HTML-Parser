22e0ff051cf43b981691eba1bc4c4b7db6cbe7fa
Antoine Sauray <sauray.antoine@gmail.com>
2016-02-18 20:03:50 +0100

    fixed indent

--- include/automate.h
+++ include/automate.h
@@ -2,20 +2,20 @@
 #define AUTOMATE_H
 
 typedef enum {
-  S0, // starting state
-  S1,
-  S2,
-  S3,
-  S4,
-  S5,
-  S6,
-  S7,
-  S8,
-  S9,
-  S10,
-  S11,
-  S12,
-  S13
+        S0, // starting state
+        S1,
+        S2,
+        S3,
+        S4,
+        S5,
+        S6,
+        S7,
+        S8,
+        S9,
+        S10,
+        S11,
+        S12,
+        S13
 } State;
 
 void run();
--- include/stack.h
+++ include/stack.h
@@ -6,39 +6,38 @@
 #include <stdlib.h>
 
 struct stack{
-  unsigned int top;
-  char stk[MAXSIZE];
+      unsigned int top;
+      char stk[MAXSIZE];
 };
 
 typedef struct stack Stack;
 
 Stack* getStack(){
-  Stack* stack;
-  stack = malloc(sizeof(Stack));
-  (*stack).top=0;
-  return stack;
+      Stack* stack;
+      stack = malloc(sizeof(Stack));
+      (*stack).top=0;
+      return stack;
 }
 
 void deleteStack(Stack* stack){
-    free(stack);
+      free(stack);
 }
 
 void push(Stack* stack, char car){
-  (*stack).stk[(*stack).top] = car;
-  printf("top=%d\n", (*stack).top);
-    (*stack).top++;
-  //printf("value[%d]=%c\n", (*stack).top-1, (*stack).stk[(*stack).top]-1);
+      (*stack).stk[(*stack).top] = car;
+      printf("top=%d\n", (*stack).top);
+      (*stack).top++;
 }
 char pop(Stack* stack){
-  char ret = (*stack).stk[(*stack).top];
-  (*stack).top--;
+       char ret = (*stack).stk[(*stack).top];
+       (*stack).top--;
 }
 void print(Stack* stack){
-  printf("printing stack\n");
-  for(int i=0;i<(*stack).top;i++){
-    printf("[%c]", (*stack).stk[i]);
-  }
-  printf("\n");
+       printf("printing stack\n");
+       for(int i=0;i<(*stack).top;i++){
+             printf("[%c]", (*stack).stk[i]);
+       }
+       printf("\n");
 }
 
 #endif
