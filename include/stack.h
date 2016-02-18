542993d260251997e06b7d672bafe8eacfa4a8d1
Antoine Sauray <sauray.antoine@gmail.com>
2016-02-18 20:00:07 +0100

    implemented stack

--- include/stack.h
+++ include/stack.h
@@ -3,20 +3,42 @@
 
 #define MAXSIZE 100
 
-struct Stack{
+#include <stdlib.h>
+
+struct stack{
+  unsigned int top;
   char stk[MAXSIZE];
-  int top;
 };
 
-typedef struct Stack Stack;
+typedef struct stack Stack;
+
+Stack* getStack(){
+  Stack* stack;
+  stack = malloc(sizeof(Stack));
+  (*stack).top=0;
+  return stack;
+}
+
+void deleteStack(Stack* stack){
+    free(stack);
+}
 
 void push(Stack* stack, char car){
   (*stack).stk[(*stack).top] = car;
-  (*stack).top++;
+  printf("top=%d\n", (*stack).top);
+    (*stack).top++;
+  //printf("value[%d]=%c\n", (*stack).top-1, (*stack).stk[(*stack).top]-1);
 }
 char pop(Stack* stack){
   char ret = (*stack).stk[(*stack).top];
   (*stack).top--;
+}
+void print(Stack* stack){
+  printf("printing stack\n");
+  for(int i=0;i<(*stack).top;i++){
+    printf("[%c]", (*stack).stk[i]);
+  }
+  printf("\n");
 }
 
 #endif
