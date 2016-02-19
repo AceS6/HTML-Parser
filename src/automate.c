e6c2483ab6ed73fb0799f81e0ac09fcb100da37e
Antoine Sauray <sauray.antoine@gmail.com>
2016-02-19 15:35:29 +0100

    trace debug

--- src/automate.c
+++ src/automate.c
@@ -4,20 +4,21 @@
 #include "../include/stack.h"
 #include "../include/io.h"
 
+bool debugState=false;
 
 void run(FILE* f){
       Stack* stack =  getStack();
       State current;  // current state
-      
+
       char c;         // current analysed caracter
       current = S0;
-      printf("STATE S0");
+      if(debugState){printf("STATE S0");}
       //char* balise="";
       while(((c = fgetc(f))!=EOF)){
-            printf("\n");
+            if(debugState){printf("\n");}
             switch(current){
                     case S0:
-                          printf("STATE S0");
+                          if(debugState){printf("STATE S0");}
                           if(c=='<'){
                               //printf("chevron ouvrant");
                               current = S1;
@@ -27,7 +28,7 @@ void run(FILE* f){
                           }
                     break;
                     case S1:
-                          printf("STATE S1");
+                          if(debugState){printf("STATE S1");}
                           if(c=='/'){
                               //printf("balise fermante\n");
                               current = S3;
@@ -42,8 +43,7 @@ void run(FILE* f){
                           }
                     break;
                     case S2:
-                          printf("STATE S2");
-                          if(c=='>'){
+                          if(debugState){printf("STATE S2");}                          if(c=='>'){
                               current = S5;
                               printf("fin de balise\n");
                           }
@@ -56,40 +56,44 @@ void run(FILE* f){
                           }
                     break;
                     case S3:
-                          printf("STATE S3");
+                          if(debugState){printf("STATE S3");}
                           if(isLetter(c) || isNumber(c)){
                               printf("\nclosing balise : caracter=%c",c);
                               current = S4;
                           }
                     break;
                     case S4:
-                          printf("STATE S4");
+                          if(debugState){printf("STATE S4");}
                           if(c=='>'){
                               current = S5;
                           }
-                          else{
+                          else if(isLetter(c) || isNumber(c)){
                                 printf("\nclosing balise : caracter=%c",c);
+                          }
+                          else{
+                                printf("error");
                           }
                     break;
                     case S5:
-                          printf("STATE S5\n");
-                          printf("automate finished");
+                          if(debugState){printf("STATE S5\n");}
+                          printf("\nPress Any Key to Continue\n");
+                          getchar();
                           run(f);
                     break;
                     case S6:
-                          printf("STATE S6");
+                          if(debugState){printf("STATE S6");}
                           if(isLetter(c) || isNumber(c)){
                               current = S7;
                           }
                     break;
                     case S7:
-                          printf("STATE S7");
+                          if(debugState){printf("STATE S7");}
                           if(c=='='){
                               current = S8;
                           }
                     break;
                     case S8:
-                          printf("STATE S8");
+                          if(debugState){printf("STATE S8");}
                           if(c=='\"'){
                               current = S10;
                           }
@@ -101,25 +105,25 @@ void run(FILE* f){
                           }
                     break;
                     case S9:
-                          printf("STATE S9");
+                          if(debugState){printf("STATE S9");}
                           if(c==' '){
                               current = S13;
                           }
                     break;
                     case S10:
-                          printf("STATE S10");
+                          if(debugState){printf("STATE S10");}
                           if(c=='\"'){
                               current = S12;
                           }
                     break;
                     case S11:
-                          printf("STATE S11");
+                          if(debugState){printf("STATE S11");}
                           if(c=='\''){
                               current = S12;
                           }
                     break;
                     case S12:
-                          printf("STATE S12");
+                          if(debugState){printf("STATE S12");}
                           if(c==' '){
                               current = S6;
                           }
@@ -128,7 +132,7 @@ void run(FILE* f){
                           }
                     break;
                     case S13:
-                          printf("STATE S13");
+                          if(debugState){printf("STATE S13");}
                           if(c=='>'){
                               current = S5;
                           }
