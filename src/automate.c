821e201c8c929f22ac1cb3481b92c258f1a55add
Antoine Sauray <sauray.antoine@gmail.com>
2016-02-20 17:09:50 +0100

    fixed issue with non closing markers

--- src/automate.c
+++ src/automate.c
@@ -11,6 +11,7 @@
 
 bool debugState=false;
 bool debugCaracter=true;
+bool step=false;
 
 void run(FILE* f){
       Stack* stack =  getStack();
@@ -18,26 +19,34 @@ void run(FILE* f){
 
       char c;         // current analysed caracter
       current = S0;
-      //char* balise="";
+      //char* marker="";
       while(((c = fgetc(f))!=EOF)){
             //printf("\e[1;1H\e[2J");
             //printf(KWHT "\n");
             if(debugCaracter){
-                  printf(KWHT "%c", c);
+                  if(debugState){
+                        printf(KWHT "\'%c\'", c);
+                  }
+                  else{
+                        printf(KWHT "%c", c);
+                  }
+
             }
             switch(current){
                     case S0:
-                          if(debugState){printf("\nSTATE S0 ");}
+                          if(debugState){printf(" STATE S0");}
                           if(c=='<'){
-                              printf(KMAG "\n_______________ balise\n");
+                              printf(KMAG "\n------------ marker");
                               current = S1;
                           }
                           else{
                               //printf("error\n");
+                              if(debugState){printf(" (skipping) ");}
                           }
+                          printf("\n");
                     break;
                     case S1:
-                          if(debugState){printf("\nSTATE S1 ");}
+                          if(debugState){printf(" STATE S1\n");}
                           appendToLastTag(stack, c);
                           if(c=='/'){
                               current = S3;
@@ -47,18 +56,18 @@ void run(FILE* f){
                           }
                           else{
                                 current = S2;
-                              //printf("balise ouvrante\n");
+                              //printf("marker ouvrante\n");
                           }
                     break;
                     case S2:
-                          if(debugState){printf("\nSTATE S2 ");}
+                          if(debugState){printf(" STATE S2\n");}
                           if(c=='>'){
                               //current = S5;
+                              printf(KMAG "\n------------ marker\n");
                               finishState(f, stack);
-                              printf("\nfin de balise\n");
                           }
                           else if(c==' '){
-                               printf(KMAG "\n_______________ balise");
+                               printf(KMAG "\n------------ marker");
                                printf(KCYN"\n======= PARAM\n");
                                current = S6;
                           }
@@ -67,48 +76,54 @@ void run(FILE* f){
                           }
                     break;
                     case S3:
-                          if(debugState){printf("\nSTATE S3 ");}
+                          if(debugState){printf(" STATE S3\n");}
                           if(isLetter(c) || isNumber(c)){
                               current = S4;
+                              appendToLastTag(stack, c);
+                          }
+                          else if(c=='>'){
+                                printf(KMAG "\n------------ marker\n");
+                               finishState(f, stack);
                           }
                     break;
                     case S4:
-                          if(debugState){printf("\nSTATE S4 ");}
+                          if(debugState){printf(" STATE S4\n");}
                           if(c=='>'){
                               //current = S5;
+                              printf(KMAG "\n------------ marker\n");
                               finishState(f, stack);
                           }
                           else if(isLetter(c) || isNumber(c)){
-
+                              appendToLastTag(stack, c);
                           }
                           else{
                                 //printf("error");
+                                appendToLastTag(stack, c);
                           }
                     break;
                     case S5:
-                          if(debugState){printf("\nSTATE S5 \n");}
+                          if(debugState){printf(" STATE S5\n");}
                           printf("\nPress Any Key to Continue\n");
                           getchar();
                           run(f);
                     break;
                     case S6:
-                          if(debugState){printf("\nSTATE S6 ");}
+                          if(debugState){printf(" STATE S6\n");}
                           if(isLetter(c) || isNumber(c)){
                                current = S7;
                           }
                           else{
                                 finishState(f, stack);
-                                printf("YOLOOOOO");
                           }
                     break;
                     case S7:
-                          if(debugState){printf("\nSTATE S7 ");}
+                          if(debugState){printf(" STATE S7\n");}
                           if(c=='='){
                                current = S8;
                           }
                     break;
                     case S8:
-                          if(debugState){printf("\nSTATE S8 ");}
+                          if(debugState){printf(" STATE S8\n");}
                           if(c=='\"'){
                                current = S10;
                           }
@@ -120,27 +135,27 @@ void run(FILE* f){
                           }
                     break;
                     case S9:
-                          if(debugState){printf("\nSTATE S9 ");}
+                          if(debugState){printf(" STATE S9\n");}
                           if(c==' '){
                               current = S13;
                           }
                     break;
                     case S10:
-                          if(debugState){printf("\nSTATE S10 ");}
+                          if(debugState){printf(" STATE S10\n");}
                           if(c=='\"'){
                                printf(KCYN "\n======= PARAM\n");
                                current = S12;
                           }
                     break;
                     case S11:
-                          if(debugState){printf("\nSTATE S11 ");}
+                          if(debugState){printf(" STATE S11\n");}
                           if(c=='\''){
-                               printf(KCYN "\n ======= PARAM\n");
+                               printf(KCYN "\n======= PARAM\n");
                                current = S12;
                           }
                     break;
                     case S12:
-                          if(debugState){printf("\nSTATE S12 ");}
+                          if(debugState){printf(" STATE S12\n");}
                           if(c==' '){
                               current = S6;
                           }
@@ -150,7 +165,7 @@ void run(FILE* f){
                           }
                     break;
                     case S13:
-                          if(debugState){printf("\nSTATE S13 ");}
+                          if(debugState){printf(" STATE S13\n");}
                           if(c=='>'){
                               //current = S5;
                               finishState(f, stack);
@@ -161,7 +176,7 @@ void run(FILE* f){
                     break;
             }
       }
-      //printf("balise=%s", balise);
+      //printf("marker=%s", marker);
       freeMemory(stack);
 }
 
@@ -173,11 +188,14 @@ void freeMemory(Stack* stack){
 }
 
 void finishState(FILE* f, Stack* stack){
+      printf("\n");
       print(stack);
       push(stack, getTag());
       if(debugState){printf("STATE S5\n");}
-      printf(KYEL "\n_______________________________________________\n");
-      printf("\nPress Any Key to Continue\n");
-      getchar();
+      printf(KYEL "\n________________________________________\n");
+      if(step){
+            printf("\nPress Any Key to Continue\n");
+            getchar();
+      }
       run(f);
 }
